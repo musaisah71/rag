eb8a6d3ce97d2b286545ba955e33a200cf45a098
Musa Isah <isahmusa71@yahoo.com>
2024-12-11 21:33:58 +0000

    folder structure added

---
+++ template.py
@@ -0,0 +1,36 @@
+# Correcting the script to use Path from pathlib and ensuring it runs without errors
+
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    ".env",
+    "requirement.txt",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+    
+    ]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Creating directory; {filedir} for the file: {filename}')
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")  
